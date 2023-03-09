bad0ea72014ed3de95a33b34c73108a6237ee1b4
David <d.elbaze@gmail.com>
2023-03-09 16:38:35 +0100

    Update Accueil.jsx

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Accueil() {
   return (
     <div>
       <nav>
-        <div>super test</div>
+        <Link to={"/"}>Liste</Link>
+        <Link to={"/create-or-edit"}>Formulaire</Link>
+        {/* <Link to={"/"}>Liste</Link>
+        <Link to={"/create-or-edit"}>Formulaire</Link> */}
       </nav>
       Accueil
       <Outlet />
