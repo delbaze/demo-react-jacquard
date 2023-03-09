778cadcbf2f45954087f28eee80a95671e8a9dbc
David <d.elbaze@gmail.com>
2023-03-09 17:11:34 +0100

    Update Liste.jsx

--- src/components/Liste.jsx
+++ src/components/Liste.jsx
@@ -1,10 +1,25 @@
-function Liste(){
-    return (
+import { useEffect } from "react";
+function Liste() {
+  useEffect(() => {
+    // fetch("http://localhost:4000/wilder/list")
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data);
+    // });
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const recupData = async () => {
+      let response = await fetch("http://localhost:4000/wilder/list", {
+        signal,
+      });
+      let data = await response.json();
+      console.log("DATA", data);
+    };
+    recupData();
 
-        <div>
-            Liste
-        </div>
-    )
+    return () => controller.abort();
+  }, []);
+  return <div>Liste</div>;
 }
 
 export default Liste;
